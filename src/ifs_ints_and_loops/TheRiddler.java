5e437a2857d91a491cc3e702f7cd3bce6efc99bf
Joshua You <JYou12@users.noreply.github.com>
2019-03-07 13:05:43 -0800

    Did some coding at home

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,4 +1,7 @@
 package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -9,16 +12,42 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
+		JOptionPane.showMessageDialog(null, "I am going to give you som riddles. \r\n Please awnser as best you can. \r\n (One Word Awnsers)");
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String riddle1 = JOptionPane.showInputDialog("Who makes it, has no need of it.\r\n" + 
+				"Who buys it, has no use for it. \r\n" + 
+				"Who uses it can neither see nor feel it. \r\n" + 
+				"What is it?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(riddle1.equalsIgnoreCase("coffin")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score += 1;
+		}else{
+			JOptionPane.showMessageDialog(null, "Wrong \r\n The awnser was coffin");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
-
+		String riddle2 = JOptionPane.showInputDialog("What can travel around the world while staying in a corner?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(riddle2.equalsIgnoreCase("stamp")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score += 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Wrong \r\n The awnser was stamp");
+		}
+		
+		String riddle3 = JOptionPane.showInputDialog("I'm tall when I'm young and I'm short when I'm old. What am I?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(riddle3.equalsIgnoreCase("candle")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score += 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Wrong \r\n The awnser was candle");
+		}
 		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "Good job you got " + score + " points!");
 		
 	}
 }
--- src/ifs_ints_and_loops/XGeeks.java
+++ src/ifs_ints_and_loops/XGeeks.java
@@ -5,6 +5,8 @@ package ifs_ints_and_loops;
 
 
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 /*
@@ -13,14 +15,34 @@ import javax.swing.JOptionPane;
 * your program should say "June's superpower is writing recipes".
 */
 public class XGeeks {
+	static boolean superpower = true;
 public static void main(String[] args) {
-
+	while (superpower == true)
+	{
+	   
+	
 		// 1. Save the superpower for each person in a variable.
-
+	int superpower = 1;
+	Random rand = new Random();
+	 int value = rand.nextInt(5); 
+	 superpower = value;
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+	String name = JOptionPane.showInputDialog("Please enter a name.");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+	if(superpower == 0) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower flight");
+	}else if(superpower == 1) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower speed");
+	}else if(superpower == 2) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower vision");
+	}else if(superpower == 3) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower super strength");
+	}else if(superpower == 4) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower is phasing");
+	}else if(name == null) {
+		System.exit(0);
 	}
+	}
+}
 }
 
